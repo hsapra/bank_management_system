25181ce9e0b529a6888a7d53573aaedf966d16ff
Hritik Sapra <hritiksapra@lawn-128-61-24-145.lawn.gatech.edu>
2022-04-21 16:55:07 -0400

    Added Database + Models

--- bank_managemtent_system/settings.py
+++ bank_managemtent_system/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'bank_managemtent_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bank_management',
+        'USER': 'root',
+        'PASSWORD': 'hello@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
