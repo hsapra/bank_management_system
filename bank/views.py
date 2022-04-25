f8080c5acbace26d9bc9928dccf3b70726f311b2
Hritik Sapra <hritiksapra@Hritiks-MacBook-Pro-3.local>
2022-04-24 23:43:24 -0400

    Done

--- bank/views.py
+++ bank/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import importlib
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 from .forms import CreateCorporationForm, CreateBankForm
-from bank.models import Corporation, Bank, Employee
+from bank.models import Corporation, Bank, Employee, Workfor
+from django.views.decorators.cache import never_cache
 
 def get_url_names():
 	from django.apps import apps
@@ -33,6 +35,7 @@ def index(request):
 	context = {'data': get_url_names()}
 	return render(request, 'bank/index.html', context)
 
+@never_cache
 def create_corporation(request):
 	if request.method == 'POST':
 		# create a form instance and populate it with data from the request:
@@ -42,16 +45,25 @@ def create_corporation(request):
 			# process the data in form.cleaned_data as required
 			# ...
 			# redirect to a new URL:
-			new_corp = Corporation(corpid = form.cleaned_data['corpID'], shortname = form.cleaned_data['shortName'], longname = form.cleaned_data['longName'], resassets = form.cleaned_data['resAssets'])
-			new_corp.save()
-			return HttpResponse(status=200)
+			if len(Corporation.objects.filter(corpid = form.cleaned_data['corpID'])) != 0 or len(Corporation.objects.filter(shortname = form.cleaned_data['shortName'])) != 0 or len(Corporation.objects.filter(longname = form.cleaned_data['longName'])) != 0:
+				messages.error(request, 'Data Exists.')
+			else:
+				new_corp = Corporation(corpid = form.cleaned_data['corpID'], shortname = form.cleaned_data['shortName'], longname = form.cleaned_data['longName'], resassets = form.cleaned_data['resAssets'])
+				new_corp.save()
+				messages.success(request, 'Corporation Created Successfully!')
+		else:
+			messages.error(request, 'Invalid form submission.')
+			messages.error(request, form.errors)
 
 	# if a GET (or any other method) we'll create a blank form
 	else:
 		form = CreateCorporationForm()
 
-	return render(request, 'bank/create_corporation.html', {'form': form})
+	context = {'form': form}
 
+	return render(request, 'bank/create_corporation.html', context)
+
+@never_cache
 def create_bank(request):
 	if request.method == 'POST':
 		# create a form instance and populate it with data from the request:
@@ -61,22 +73,27 @@ def create_bank(request):
 			# process the data in form.cleaned_data as required
 			# ...
 			# redirect to a new URL:
-			managerId = form.cleaned_data['manager']
-
-			if len(Bank.objects.filter(manager_id = managerId)) == 0:
-				new_bank = Bank(
-					bankid = form.cleaned_data['bankID'], 
-					bankname = form.cleaned_data['bankName'], 
-					street = form.cleaned_data['street'], 
-					city = form.cleaned_data['city'], 
-					state = form.cleaned_data['state'], 
-					zip = form.cleaned_data['zip_code'], 
-					resassets = form.cleaned_data['resAssets'],
-					corpid = Corporation.objects.get(pk = form.cleaned_data['corpID']),
-					manager = Employee.objects.get(pk=form.cleaned_data['manager']))
-				new_bank.save()
-			return HttpResponse(status=200)
-
+			if len(Bank.objects.filter(manager_id = form.cleaned_data['manager'])) == 0:
+				if len(Bank.objects.filter(manager_id = form.cleaned_data['bank_employee'])) == 0:
+					if len(Bank.objects.filter(bankid = form.cleaned_data['bankID'])) == 0:
+						new_bank = Bank(
+							bankid = form.cleaned_data['bankID'], 
+							bankname = form.cleaned_data['bankName'], 
+							street = form.cleaned_data['street'], 
+							city = form.cleaned_data['city'], 
+							state = form.cleaned_data['state'], 
+							zip = form.cleaned_data['zip_code'], 
+							resassets = form.cleaned_data['resAssets'],
+							corpid = Corporation.objects.get(pk = form.cleaned_data['corpID']),
+							manager = Employee.objects.get(pk=form.cleaned_data['manager']))
+						new_bank.save()
+					else:
+						messages.error(request, 'Existing Bank.')
+				else:
+					messages.error(request, 'Employee cannot be existing manager.')
+			else:
+				messages.error(request, 'Manager cannot be existing manager.')
+				
 	# if a GET (or any other method) we'll create a blank form
 	else:
 		form = CreateBankForm()
--- bank_managemtent_system/settings.py
+++ bank_managemtent_system/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import pymysql
+from django.contrib.messages import constants as messages
+
 pymysql.install_as_MySQLdb()
 
 
@@ -107,6 +109,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
